f36fc842a8dc586aa48c070f95c7ff8e352d20fb
egorosi123 <31862491+egorosi123@users.noreply.github.com>
2022-11-14 10:31:18 +0300

    Add files via upload

---
+++ 21.2/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = 11;
+            double[] Array = new double[size];
+            textBox1.Text = "";
+            textBox2.Text = "";
+            for (int i = 0; i < Array.Length; i++)
+            {
+                Array[i] = Math.Pow(2, i);
+                textBox1.Text+=Array[i]+" ";
+            }
+            for (int i = size-1; i >= 0; i--)
+            {
+                textBox2.Text += Array[i]+" ";
+            }
+        }
+
+    }
+}
